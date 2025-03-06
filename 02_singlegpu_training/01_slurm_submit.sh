e1ae7300804ac48b442aab3987718a064a9a56cf
kishor yd <114437235+kishoryd@users.noreply.github.com>
2025-03-06 23:36:16 +0530

    Update 01_slurm_submit.sh

--- 02_singlegpu_training/01_slurm_submit.sh
+++ 02_singlegpu_training/01_slurm_submit.sh
@@ -4,8 +4,7 @@
 #SBATCH --ntasks=1               # total number of tasks across all nodes
 #SBATCH --cpus-per-task=1        # cpu-cores per task (>1 if multi-threaded tasks)
 #SBATCH --partition=gpu          # Specify GPU partition for GPU Nodes
-#SBATCH --reservation=hpcai      # Reservation incase of urgent requirement
-##SBATCH --nodelist=rpgpu*        # Specify reservation GPU node name provided
+#SBATCH --reservation=SCA        # Reservation incase of urgent requirement
 #SBATCH --gres=gpu:1             # number of gpus per node
 #SBATCH --output=logs_%j.out     # output logfile name
 #SBATCH --error=logs_%j.err      # error logfile name
@@ -20,10 +19,6 @@ module purge #remove unneccesary loaded modules
 module load miniconda
 
 #activate the environment
-conda activate gujcost_workshop
+conda activate tutorial
 
-#Try both the option, See the performance
-
-kernprof -o ${SLURM_JOBID}_${SLURM_CPUS_PER_TASK}.lprof -l mnist_model.py --epochs=5
-
-#kernprof -o ${SLURM_JOBID}_${SLURM_CPUS_PER_TASK}.lprof -l mnist_model.py --epochs=5 --no-cuda
+time python 01_mnist_model.py --epochs=6
