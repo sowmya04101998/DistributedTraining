517fa6feadcf5788a952c84de637df915f4c59de
kishor yd <114437235+kishoryd@users.noreply.github.com>
2025-03-07 18:19:37 +0530

    Update and rename NGC_dp_2GPU.sh to ngc_multigpu.sh

--- 06_DL_container/ngc_multigpu.sh
+++ 06_DL_container/ngc_multigpu.sh
@@ -11,4 +11,4 @@
 time srun --container-image=/home/apps/enroot/nvidia+pytorch+24.11-py3.sqsh \
         --container-name=pytorch \
 --container-mounts=$HOME/SCA-2025-DistributedTraining/03_multigpu_dp_training/:/workspace,/var/share/slurm/slurm.taskprolog:/var/share/slurm/slurm.taskprolog \
-        sh -c 'cd /workspace && python mnist_multigpu.py --epochs=5'
+        sh -c 'cd /workspace && python mnist_multigpu.py --epochs=10'
