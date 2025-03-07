ac213e292974068086b56c5fc3b2e427776bd4c7
kishor yd <114437235+kishoryd@users.noreply.github.com>
2025-03-07 18:06:30 +0530

    Create NGC_mnist_1GPU.sh

---
+++ 06_DL_container/NGC_mnist_1GPU.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --gres=gpu:1
+#SBATCH --partition=gpu
+#SBATCH --time=1:00:00
+#SBATCH --exclusive
+#SBATCH --output=%j.out
+#SBATCH --error=%j.err
+#SBATCH --reservation=SCA
+
+time srun --container-image=/home/apps/enroot/nvidia+pytorch+24.11-py3.sqsh \
+        --container-name=pytorch \
+--container-mounts=$HOME/SCA-2025-DistributedTraining/02_singlegpu_training/:/workspace,/var/share/slurm/slurm.taskprolog:/var/share/slurm/slurm.taskprolog \
+        sh -c 'cd /workspace && python mnist_model.py --epochs=5'
