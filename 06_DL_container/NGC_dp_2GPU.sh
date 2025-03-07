f156cb07fb5af67dae0535fb55ef7bb5b81fa675
kishor yd <114437235+kishoryd@users.noreply.github.com>
2025-03-07 18:07:27 +0530

    Create NGC_dp_2GPU.sh

---
+++ 06_DL_container/NGC_dp_2GPU.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --gres=gpu:2
+#SBATCH --partition=gpu
+#SBATCH --time=1:00:00
+#SBATCH --exclusive
+#SBATCH --output=%j.out
+#SBATCH --error=%j.err
+#SBATCH --reservation=SCA
+
+time srun --container-image=/home/apps/enroot/nvidia+pytorch+24.11-py3.sqsh \
+        --container-name=pytorch \
+--container-mounts=$HOME/SCA-2025-DistributedTraining/03_multigpu_dp_training/:/workspace,/var/share/slurm/slurm.taskprolog:/var/share/slurm/slurm.taskprolog \
+        sh -c 'cd /workspace && python mnist_multigpu.py --epochs=5'
