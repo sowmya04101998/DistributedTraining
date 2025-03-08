927e2d0338e746cc7c9e2f493044d8889fefc567
kishor yd <114437235+kishoryd@users.noreply.github.com>
2025-03-09 01:12:21 +0530

    Update 03_mnist_AMP.sh

--- 02_singlegpu_training/03_mnist_AMP.sh
+++ 02_singlegpu_training/03_mnist_AMP.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=mnist_1GPU    # create a short name for your job
+#SBATCH --job-name=mnist_AMP     # create a short name for your job
 #SBATCH --nodes=1                # node count
 #SBATCH --ntasks=1               # total number of tasks across all nodes
 #SBATCH --cpus-per-task=1        # cpu-cores per task (>1 if multi-threaded tasks)
@@ -21,4 +21,4 @@ module load miniconda
 #activate the environment
 conda activate tutorial
 
-time python 01_mnist_AMP.py --epochs=6
+time python 01_mnist_AMP.py --epochs=10
