502081e648c0bdee853cdd634654656541588c8a
kishor yd <114437235+kishoryd@users.noreply.github.com>
2025-03-11 21:03:49 +0530

    Update 01_mnist_model.sh

--- 06_DL_container/01_mnist_model.sh
+++ 06_DL_container/01_mnist_model.sh
@@ -10,5 +10,5 @@
 
 time srun --container-image=/home/apps/enroot/nvidia+pytorch+24.11-py3.sqsh \
         --container-name=pytorch \
---container-mounts=$HOME/SCA-2025-DistributedTraining/02_singlegpu_training/:/workspace,/var/share/slurm/slurm.taskprolog:/var/share/slurm/slurm.taskprolog \
+--container-mounts=$HOME/SCA-2025-DistributedTraining/06_DL_container/:/workspace,/var/share/slurm/slurm.taskprolog:/var/share/slurm/slurm.taskprolog \
         sh -c 'cd /workspace && python 01_mnist_model.py --epochs=5'
