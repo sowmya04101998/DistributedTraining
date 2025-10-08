178b37d54e4646861034c9dd5e682941929833e5
sowmya04101998 <133966292+sowmya04101998@users.noreply.github.com>
2025-10-08 18:44:15 +0530

    Delete

--- 05_multigpu_fsdp_training/downloadmodel.py
+++
@@ -1,4 +0,0 @@
-from transformers import AutoTokenizer, AutoModelForCausalLM
-
-AutoTokenizer.from_pretrained("codellama/CodeLlama-7b-hf", cache_dir=".cache")
-AutoModelForCausalLM.from_pretrained("codellama/CodeLlama-7b-hf", cache_dir=".cache")
